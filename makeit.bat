3917e2b880ea54cb30d48be3d3ae2bdac3239fdd
Nalle Berg <nalle@nalle.no>
2025-12-14 08:15:04 +0200

    New test

--- makeit.bat
+++ makeit.bat
@@ -3,19 +3,17 @@ cls
 REM Remove old build directory
 rmdir /s /q build
 
-REM Configure the project for Visual Studio 2022 x64
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64
+REM Configure the project for Visual Studio 2022 x64, with Qt6 CMake path
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH="C:\Qt\6.9.1\msvc2022_64\lib\cmake"
 
 REM Build in Release mode
 cmake --build build --config Release --verbose
 
 REM Deploy Qt dependencies
-C:\Qt\6.9.1\msvc2022_64\bin\windeployqt.exe  .\build\Release\IPGUI.exe
-
+C:\Qt\6.9.1\msvc2022_64\bin\windeployqt.exe .\build\Release\IPGUI.exe
 
 REM Copying Standard Working gif to the build directory
 copy .\StdWorking.gif .\build\Release\StdWorking.gif
-
 
 REM Renaming the build/Release directory to IPGui
 ren .\build\Release IPGui
